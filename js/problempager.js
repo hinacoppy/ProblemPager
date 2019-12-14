3c7b9dea51576d231fa51f9af578aa20f3c508e1
hinacoppy <kawakami@js4.so-net.ne.jp>
2019-12-14 15:56:53 +0900

    display navigation buttons anytime

--- js/problempager.js
+++ js/problempager.js
@@ -1,11 +1,10 @@
 // problempager.js
-// include from problempager html (html/{CAT}/{NUM}.html)
+// include from problempager html (ProblemPager/[C]/{CAT}/{NUM}.html)
 
 //回答、解説を最初は非表示にする
 $('.answerscore').hide();
 $('.description').hide();
 $('.gnuanalysis').hide();
-$('.nav').hide();
 if (!showpipflg) { $('.pipinfo').hide(); }
 
 //範囲を超えて移動できないようにする
@@ -59,20 +58,19 @@ $(function() {
       $('#gnuanalysis').toggle(); //SpaceキーはGnuBG解析結果の表示/非表示を切替え
       break;
     default:
-      return; //何もしない
+      return false; //何もしない
     }
+    return false;
   });
 
   //[Description]ボタンか、ボードのクリックで、回答、解説の表示/非表示を切替え
   $('#showanswer, .mainboard').on('click', function(e){
     description("toggle");
-    $('.nav').slideUp(300);
   });
 
   //[Analysis Result]ボタンで、解析結果を表示/非表示を切替え
   $('#analyse').on('click', function(e){
     $('#gnuanalysis').toggle();
-    $('.nav').slideUp(300);
   });
 
   //[Home]ボタンで、メニューに遷移
@@ -80,15 +78,9 @@ $(function() {
     window.location.href = "../../index.html";
   });
 
-  //[Option]ボタンで、ナビゲーションメニューをトグル
-  $('#option').on('click', function(e){
-    $('.nav').slideToggle(300);
-  });
-
 });
 
 function move_page(probnum, delta) {
-  $('.nav').slideUp(1000);
   let nextpage = Number(probnum) + delta;
   if (nextpage <= 0 || nextpage > 50) { return; } 
   let pn = ("00" + String(nextpage)).substr(-2);
